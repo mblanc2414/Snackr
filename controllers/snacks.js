a6d520375b5f15fff1b86853b623b2a295449b45
mblanc2414 <mblanc2414@gmail.com>
2022-05-28 18:39:03 -0400

    controllers Routes finished

--- controllers/snacks.js
+++ controllers/snacks.js
@@ -16,18 +16,44 @@ router.get('/', (req, res) => {
     });
 });
 //////////////////////// NEW ////////////////////////
-
+router.get('/new', (req, res) => {
+    res.render('snacks/new.ejs');
+});
 
 //////////////////////// DELETE ////////////////////////
-
+router.delete('/:id', (req, res) => {
+    Snack.findByIdAndRemove(req.params.id, () => {
+        res.redirect('/snacks');
+    })
+})
 
 //////////////////////// UPDATE ////////////////////////
-
+router.put('/:id', (req, res) => {
+    Snack.findByIdAndUpdate(req.params.id, req.body, () => {
+        res.redirect('/snacks');
+    });
+});
 
 //////////////////////// CREATE ////////////////////////
-
+router.post('/', (req, res) => {
+    Snack.create(req.body, (err, createdSnack) => {
+        res.redirect('/snacks');
+    });
+});
 //////////////////////// EDIT ////////////////////////
-
+router.get('/:id/edit', (req, res) => {
+    Snack.findById(req.params.id, (err, foundSnack) => {
+        res.render('snacks/edit.ejs', {
+            snack: foundSnack,
+        });
+     });
+});
 
 //////////////////////// SHOW ////////////////////////////
-
+router.get('/:id', (req, res) => {
+    Snack.findById(req.params.id, (err, foundSnack) => {
+        res.render('snacks/show.ejs', {
+            snack: foundSnack,
+        });
+    });
+});
