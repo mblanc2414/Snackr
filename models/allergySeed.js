c3ce2695ca86880eb59e8d6e9526d3c84abddd38
mblanc2414 <mblanc2414@gmail.com>
2022-05-29 14:26:30 -0400

    Category Seed Databases Added

---
+++ models/allergySeed.js
@@ -0,0 +1,22 @@
+// Allergy Friendly Category
+module.exports = [
+    {
+      name: 'Beans',
+      brand:
+        'A small pile of beans. Buy more beans for a big pile of beans.',
+      img: 'https://imgur.com/LEHS8h3.png',
+      category: ''
+    },
+    {
+      name: 'Bones',
+      brand: "It's just a bag of bones.",
+      img: 'https://imgur.com/dalOqwk.png',
+      category: '',
+    },
+    {
+      name: 'Bins',
+      brand: 'A stack of colorful bins for your beans and bones.',
+      img: 'https://imgur.com/ptWDPO1.png',
+      category: '',
+    }
+  ];
